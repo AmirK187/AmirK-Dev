4af48cd8e021ac60c313d8952a9e07915dea9777
AmirK <kooverjeeamir@gmail.com>
2019-08-14 22:19:22 +0200

    Making progress for new portfolio page.

--- fullPage.js
+++ fullPage.js
@@ -2,6 +2,12 @@ new fullpage('#fullpage', {
 	//options here
     autoScrolling:true,
     scrollingSpeed:2000,
-	scrollHorizontally: true
+    navigation: true,
+	navigationPosition: 'left',
+	navigationTooltips: ['Home', 'About','Pojects','Contact'],
+    scrollHorizontally: true,
+    showActiveTooltip: true,
 });
+
+
 
